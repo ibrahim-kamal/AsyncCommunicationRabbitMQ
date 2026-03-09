0c55c44f9cf8747fa31871b23a8d73a4db9b0c64
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2026-03-09 10:39:33 +0200

    Add project files.

---
+++ AsyncCommunication-Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+async Task Example3() {
+    var factory = new ConnectionFactory(){ HostName = "localhost" };
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+
+    consumer.ReceivedAsync += async (model, ea) => {
+        var body = ea.Body.ToArray();
+        
+        var message = Encoding.UTF8.GetString(body);
+
+        //await channel.BasicRejectAsync(ea.DeliveryTag, false);
+
+        Console.WriteLine(message);
+    };
+
+    await channel.BasicConsumeAsync("dead-Queue", false, consumer);
+
+    Console.ReadLine();
+
+
+
+}
+
+await Example3();
---
+++ AsyncCommunicationRabbitMQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+async Task example3() {
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync(
+        new CreateChannelOptions(publisherConfirmationsEnabled: true, publisherConfirmationTrackingEnabled: false)
+    );
+    // publisherConfirmationsEnabled To acknowledgments  
+    // publisherConfirmationTrackingEnabled if true that will give exception if there any exception,
+    // you can use BasicReturnAsync event to handle exception [Exception happened from exchange but if exchange not found that will not return exception]
+
+    var message = "Hello Developer!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicReturnAsync += async (sender, args) => 
+    {
+        Console.WriteLine("{0} Reply Text {1}" , args.RoutingKey , args.ReplyText);
+        Console.WriteLine("-------------------------------------------------------");
+        await Task.CompletedTask;
+    };
+
+    channel.ChannelShutdownAsync += async (sender, args) => 
+    {
+        Console.WriteLine("Reply Text {0}" ,  args.ReplyText);
+        Console.WriteLine("-------------------------------------------------------");
+        await Task.CompletedTask;
+    };
+    channel.BasicAcksAsync += async (sender, args) => // publisherConfirmationsEnabled
+    {
+        Console.WriteLine("Delivery Tag ");
+        Console.WriteLine("-------------------------------------------------------");
+        await Task.CompletedTask;
+    };
+
+
+
+    await channel.BasicPublishAsync(exchange: "amq.fanout", routingKey: "Key111",  body: body, mandatory: true);
+
+
+    Console.ReadLine();
+
+}
+
+await example3();
